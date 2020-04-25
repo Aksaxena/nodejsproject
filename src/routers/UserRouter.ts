8938903f6cade3d00906488e07c2903671442926
aksaxena <saxenaalokkumar878@gmail.com>
2020-04-25 05:44:13 +0530

    Another code commited

---
+++ src/routers/UserRouter.ts
@@ -0,0 +1,34 @@
+import {Router} from 'express';
+import {UserController} from '../controllers/UserController';
+import {UserValidators} from '../validators/UserValidators';
+import {GlobalMiddleWare} from '../middlewares/GlobalMiddleWare';
+
+class UserRouter {
+    public router: Router;
+
+    constructor() {
+        this.router = Router();
+        this.getRoutes();
+        this.postRoutes();
+        this.patchRoutes();
+        this.deleteRoutes();
+    }
+
+    getRoutes() {
+        this.router.get('/send/verification/email', UserValidators.resendVerificationEmail(),UserController.resendVerificationEmail)
+    }
+
+    postRoutes() {
+        this.router.post('/signup', UserValidators.signUp(), GlobalMiddleWare.checkError, UserController.signUp);
+    }
+
+    patchRoutes() {
+        this.router.patch('/verify', UserValidators.verifyUser(), GlobalMiddleWare.checkError, UserController.verify);
+    }
+
+    deleteRoutes() {
+
+    }
+}
+
+export default new UserRouter().router;
